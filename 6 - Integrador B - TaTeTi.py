b5e54b6fd359fe4c5acf7771a38c07afc6c24606
enriquemgiunta <enriquemgiunta@gmail.com>
2023-11-15 18:06:13 -0300

    Integrador incompleto aun.

--- 6 - Integrador B - TaTeTi.py
+++ 6 - Integrador B - TaTeTi.py
@@ -3,36 +3,47 @@ def jugada_maquina():
     pass
 
 def jugada_humano():
-    pos_humano = "."
-    while pos_humano not in tablero:
-        pos_humano = input("Ingrese la posición dónde colocar su jugada: (1 - 9)")
-        if pos_humano < "1" or pos_humano > "9":
-            print("Debe ingresar un valor entre 1 y 9.")
-    print(f"índice de la posición es: {tablero.index(pos_humano)}")
-    tablero[tablero.index(pos_humano)] = ficha_humano
+    siguiente = False
+    while siguiente == False:
+        pos_humano = input("Ingrese el número de casillero donde colocar su jugada: (1 - 9)")
+        if pos_humano in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            if coloca_ficha(pos_humano, ficha_humano):
+                siguiente = True
+        else:
+            print("Ingrese un número del 1 al 9.")
+
+def coloca_ficha(_pos, _ficha):    
+    salida = False
+    for y in range(3):
+        for x in range(3):
+            print(f"tablero[{y}][{x}] tiene {tablero[y][x]} = {_pos} ?")
+            if _pos == tablero[y][x]:
+                tablero[y][x] = _ficha
+                salida = True
+                break
+    print(f"Salida al colocar ficha={salida}")
     dibuja_tablero()
-    revisa_estado()
+    return salida    
     
 def dibuja_tablero():
-    print("     A   B   C ")
-    print("   -------------")
-    print(f"1  I {tablero[0,0]} I {tablero[0,1]} I {tablero[0,2]} I")
-    print("   -------------")
-    print(f"2  I {tablero[1,0]} I {tablero[1,1]} I {tablero[1,2]} I")
-    print("   -------------")
-    print(f"3  I {tablero[2,0]} I {tablero[2,1]} I {tablero[2,2]} I")
-    print("   -------------")
+    print("┌───┬───┬───┐")
+    print(f"│ {tablero[0][0]} │ {tablero[0][1]} │ {tablero[0][2]} │")
+    print("├───┼───┼───┤")
+    print(f"│ {tablero[1][0]} │ {tablero[1][1]} │ {tablero[1][2]} │")
+    print("├───┼───┼───┤")
+    print(f"│ {tablero[2][0]} │ {tablero[2][1]} │ {tablero[2][2]} │")
+    print("└───┴───┴───┘")
 
 def revisa_estado(_jugador):
     ganador = " "
     for x in range(3):
-        if _jugador == tablero[0, x] and _jugador == tablero[1, x] and _jugador == tablero[2, x]:
+        if _jugador == tablero[0][x] and _jugador == tablero[1][x] and _jugador == tablero[2][x]:
             ganador = _jugador
-        elif _jugador == tablero[x, 0] and _jugador == tablero[x, 1] and _jugador == tablero[x, 2]:
+        elif _jugador == tablero[x][0] and _jugador == tablero[x][1] and _jugador == tablero[x][2]:
             ganador = _jugador
-    if _jugador == tablero[0, 0] and _jugador == tablero[1, 1] and _jugador == tablero[2, 2]:
+    if _jugador == tablero[0][0] and _jugador == tablero[1][1] and _jugador == tablero[2][2]:
         ganador = _jugador
-    if _jugador == tablero[2, 0] and _jugador == tablero[1, 1] and _jugador == tablero[0, 2]:
+    if _jugador == tablero[2][0] and _jugador == tablero[1][1] and _jugador == tablero[0][2]:
         ganador = _jugador
     return ganador
 
@@ -42,7 +53,16 @@ gano = "."
 numero_aleatorio = randint(0, 18)
 print("Ta Te Ti")
 ficha_humano = input("Elija un símbolo para su jugador (x o):")
+if ficha_humano == "x":
+    ficha_maquina = "o"
+else:
+    ficha_maquina = "x"
+orden = randint(1, 2)
 while(gano != "o" and gano != "x"):
     dibuja_tablero()
-    jugada_humano()
-    jugada_maquina()
+    if orden == 1:
+        jugada_humano()
+        jugada_maquina()
+    else:
+        jugada_maquina()
+        jugada_humano()

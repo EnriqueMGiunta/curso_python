53d52faa46e37bb4ad8099f9e5de1ee743a2965d
enriquemgiunta <enriquemgiunta@gmail.com>
2023-11-22 16:34:44 -0300

    Reordenados.

---
+++ A - Integrador A - TaTeTi.py
@@ -0,0 +1,92 @@
+# TA TE TI
+def jugada_maquina():
+    siguiente = False
+    while siguiente == False:
+        pos_maquina = str(randint(1, 9))
+        print(f"Jugada máquina = \"{pos_maquina}\"")
+        if coloca_ficha(pos_maquina, ficha_maquina):
+            siguiente = True
+    return
+
+def jugada_humano():
+    siguiente = False
+    while siguiente == False:
+        pos_humano = input("Ingrese el número de casillero donde colocar su jugada: (1 - 9)")
+        if pos_humano in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            if coloca_ficha(pos_humano, ficha_humano):
+                siguiente = True
+        else:
+            print("Ingrese un número del 1 al 9:")
+    return
+
+def coloca_ficha(_pos, _ficha):    
+    salida = False
+    for y in range(3):
+        for x in range(3):
+            if _pos == tablero[y][x]:
+                tablero[y][x] = _ficha
+                dibuja_tablero()
+                salida = True
+                break
+    return salida    
+
+def dibuja_tablero():
+    print("┌───┬───┬───┐")
+    print(f"│ {tablero[0][0]} │ {tablero[0][1]} │ {tablero[0][2]} │")
+    print("├───┼───┼───┤")
+    print(f"│ {tablero[1][0]} │ {tablero[1][1]} │ {tablero[1][2]} │")
+    print("├───┼───┼───┤")
+    print(f"│ {tablero[2][0]} │ {tablero[2][1]} │ {tablero[2][2]} │")
+    print("└───┴───┴───┘")
+
+def revisa_estado():
+    ganador = " "
+    for ficha in ["x", "o"]:
+        for x in range(3):
+            if ficha == tablero[0][x] and ficha == tablero[1][x] and ficha == tablero[2][x]:
+                ganador = ficha
+            elif ficha == tablero[x][0] and ficha == tablero[x][1] and ficha == tablero[x][2]:
+                ganador = ficha
+        if ficha == tablero[0][0] and ficha == tablero[1][1] and ficha == tablero[2][2]:
+            ganador = ficha
+        if ficha == tablero[2][0] and ficha == tablero[1][1] and ficha == tablero[0][2]:
+            ganador = ficha
+    return ganador
+
+
+# ******* Bloque principal ********
+from random import randint
+nuevo_juego = "s"
+ficha_por_omision = "x"
+while nuevo_juego == "s":
+    tablero = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+    gano = "."
+    jugada = 0
+    orden = randint(0, 1)
+
+    print("\n\n\nTa Te Ti")
+    ficha_humano = input(f"Elija un símbolo para su jugador \"o\" o \"x\" ({ficha_por_omision}):")
+    if ficha_humano != "x" and ficha_humano != "o":
+        ficha_humano = ficha_por_omision
+    if ficha_humano == "x":
+        ficha_maquina = "o"
+    else:
+        ficha_maquina = "x"
+    ficha_por_omision = ficha_humano
+    while gano != "o" and gano != "x" and jugada < 8:
+        dibuja_tablero()
+        if (jugada + orden)%2 == 0:
+            jugada_humano()
+            gano = revisa_estado()
+        else:
+            jugada_maquina()
+            gano = revisa_estado()
+        jugada += 1
+
+    if gano == ficha_humano:
+        print(f"\nGanador: \"EL HUMANO\", felicitaciones!!!\n\n")
+    elif gano == ficha_maquina:
+        print(f"\nGanador: \"LA COMPUTADORA\", felicitaciones a mí!!!\n\n")
+    else:
+        print(f"\nEsto fue un empate.\n\n")
+    nuevo_juego = input(f"\nJugamos de nuevo (s / n)? ")
---
+++ A - Integrador B - Cajero.py
@@ -0,0 +1,96 @@
+# Simulador de cajero automático
+# <enriquemgiunta@gmail.com>
+#┌─┬─┐
+#│ │ │
+#├─┼─┤
+#└─┴─┘
+
+def menu_principal():
+    seleccion = 0
+    while seleccion <= 0 or seleccion > len(cuentas):
+        print(f"""
+{margen}┌────────────────────────────┐
+{margen}│  CAJERO AUTOMÁTICO VIRTUAL │
+{margen}└────────────────────────────┘
+
+{margen}  Cuentas disponibles:\n""")
+        for y in range(len(cuentas)):
+            print(f"{margen}  {y + 1} - {cuentas[y][0]}")
+        print(f"\n{margen}  9 - Salir.")
+        seleccion = int(input(f"\n{margen}Elija con qué cuenta trabajar (1 - {y + 1}): "))
+        if seleccion != 99:
+            print(f"{margen}Cuenta seleccionada: {cuentas[seleccion - 1][0]}\n")
+    return seleccion - 1
+
+def titulo_cuenta(_cuenta):
+    print(f"{margen}┌────────────────────────────┐")
+    print(f"{margen}│   CUENTA: \"{cuentas[_cuenta][0]}\"")
+    print(f"{margen}└────────────────────────────┘\n")
+    return
+
+def login(_cuenta):
+    intentos = 3
+    titulo_cuenta(_cuenta)
+    while intentos >= 0:
+        pw = input(f"\n{margen}   Ingrese su contraseña de acceso: ")    
+        if pw != cuentas[_cuenta][1]:
+            print(f"\n{margen}   Contraseña incorrecta, le quedan {intentos} intentos.")
+            intentos -= 1
+        else:
+            return True
+    return False
+
+def consultar_saldo(_cuenta):
+    titulo_cuenta(_cuenta)
+    print(f"{margen}   El saldo de la cuenta es de ${cuentas[_cuenta][2]}.")
+    input(f"\n{margen}   Pulse enter para volver al menú.")
+    return
+
+def ingresar_dinero(_cuenta):
+    titulo_cuenta(_cuenta)
+    monto = -1
+    type(monto)
+    while monto < 0 or type(monto) != int:
+        monto = int(input(f"\n{margen}   Cantidad de pesos argentinos (AR$) a ingresar? (0 para salir): "))
+        if monto == 0:
+            return False
+        elif monto < 0:
+            print(f"{margen}   El monto no puede ser negativo.")
+        elif type(monto) != "int":
+            print(f"{margen}   El monto debe ser un número.")
+    cuentas[_cuenta][2] += monto
+    print(f"\n{margen}   Se ingresó la suma de ${monto} a la cuenta \"{cuentas[_cuenta][0]}\".")
+    print(f"\n{margen}   El nuevo saldo es de ${cuentas[_cuenta][2]}.")
+    return True
+
+def menu_cuenta(_cuenta):
+    operacion = 0
+    while operacion != 9:
+        titulo_cuenta(_cuenta)
+        print(f"{margen}   1 - Consultar saldo.")
+        print(f"{margen}   2 - Ingresar dinero.")
+        print(f"{margen}   3 - Retirar dinero.")
+        print(f"{margen}   4 - Transferir dinero.")
+        print(f"{margen}   9 - Salir.")
+        operacion = int(input(f"\n{margen}Elija una opción."))
+        if operacion == 1:
+            consultar_saldo(_cuenta)
+        elif operacion == 2:
+            cuentas[_cuenta][2] += ingresar_dinero(_cuenta)
+    return
+
+# *************** Bloque principal *****************
+cuentas = [
+        ["Mario", "Paasswwoorrddsss", 300000],
+        ["Raquel", "Otra_clave", 320000],
+        ["Camila", "Ad1v1n4", 45000],
+        ["Sol", "Malbec", 5000]
+        ]
+salir = False
+margen = "          "
+cuenta_elegida = menu_principal()
+while cuenta_elegida != 8:
+    if login(cuenta_elegida):
+        menu_cuenta(cuenta_elegida)
+        menu_principal()
+print(f"{margen}Gracias por usar Cajero Virtual.")
